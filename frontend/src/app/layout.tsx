eb16f59fa00ba4c1b03c2d2a1b34c3ae40ce686b
j-mahapatra <jagannathm.official@gmail.com>
2025-08-21 18:16:49 +0530

    frontend initial setup

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import "@/styles/globals.css";
+
+import { type Metadata } from "next";
+import { Geist } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "Music Generation App",
+  description: "Generate Music using AI",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${geist.variable} antialiased`}>
+      <body>{children}</body>
+    </html>
+  );
+}
